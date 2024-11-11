3191837e10eb57dd1cc508bf588fb849226bc642
ashwinn-si <ashwin23cse@gmail.com>
2024-11-11 20:07:50 +0530

    bug killed

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let result_mark=[0,0,false,false,false,false,0,0];//first_10,second_10,bonus,nptel,course,extra,final,external
 
-let nptel_pop_flag=false;
-
 function input_box_error_handler(mark,element_id){
     if(mark<0 || mark>100|| isNaN(mark)){
         navigator.vibrate(200);
@@ -71,13 +69,8 @@ document.getElementById("calculate_button").addEventListener('click',()=>{
         })
         main(m1_mark,m2_mark,m3_mark);
         celebration_effect();
-        if(!nptel_pop_flag){
-            document.getElementById('nptel_alert_container').style.visibility='visible';
-            nptel_pop_flag=true;
-        }
+        document.getElementById('nptel_alert_container').style.visibility='visible';
     }
-    
-    
 })
 
 function mark_calculator(m1_mark,m2_mark,m3_mark,bonus_flag){
@@ -188,7 +181,7 @@ function display_changer(){
 }
 function main(m1_mark,m2_mark,m3_mark){
     mark_calculator(m1_mark,m2_mark,m3_mark,result_mark[2]);
-    if(m1_mark+m2_mark+m3_mark>100){
+    if(m1_mark+m2_mark+m3_mark>=100){
         extra_activity_cal();
     }else{
         document.getElementById('special_case_text').innerHTML=`<p style=" background-color: tomato;width: 150px; padding: 3px;border-radius: 5px; font-weight: bold; color: white; box-shadow: 0 2px 4px rgba(0, 0, 0, 1);">SPECIAL CASE</p>`;
