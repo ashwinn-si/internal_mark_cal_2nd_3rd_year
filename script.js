596a5550dcfd81767790721245f017a2e9917825
Abdul Aziz <abdulaziz87049@gmail.com>
2025-04-18 12:14:23 +0530

    Bhaii branch (#1)
    * first commit
    
    * nptel 0 4 8 done, result array will have nptel mark directly instead of true/false
    
    * course and extra wins will now be represented as integers in result_mark instead of true/false
    
    ---------
    
    Co-authored-by: ashwinn-si <ashwin23cse@gmail.com>

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-let result_mark=[0,0,false,false,false,false,0,0];//first_10,second_10,bonus,nptel,course,extra,final,external
+/*
+ changes made => 1. result_mark will now have live marks(0, 4, 8) for nptel, course and extra wins
+                    instead of having true/ false
+                 2. radio_button_checker can now take variable number of arguements. (2 or more)
+                    initally it only took 2 arguements.
+                 3. changes were made in internal_mark_calculation() to reflect on the new result_mark style  
+                
+                 4. changes were made in display_change(), to display appropriate results. 
+                    (previously, it checked the boolean value of the array, now it will update the result straight ah) 
+                 */         
+
+
+let result_mark=[0, 0, 1, 0, 0, 0, 0, 0];//first_10,second_10,bonus,nptel,course,extra,final,external
+/* 
+    0 -> first_10 [0 to 10]
+    1 -> second_10 [0 to 10]
+    2 -> bonus [1 or 1.5]
+    3 -> NPTEL [0 or 4 or 8]
+    4 -> course [0 or 7]
+    5 -> extra (competiton win or mini project) [0 or 5]
+*/
 
 function input_box_error_handler(mark,element_id){
     if(mark<0 || mark>100|| isNaN(mark)){
@@ -13,22 +33,25 @@ function input_box_error_handler(mark,element_id){
     return true;
 }
 
-function radio_button_checker(b1, b2) {
-    const button1 = document.getElementById(b1);
-    const button2 = document.getElementById(b2);
+function radio_button_checker(...args) {
+    let isINVALID = true;
 
-    if (!button1.checked && !button2.checked) {
+    for(let i=0; i<args.length; i++){   // check if any button is checked
+        if(document.getElementById(args[i]).checked){
+            isINVALID = false;
+            break;
+        }
+    }
+
+    if(isINVALID){  // enters when there is button checked.
         navigator.vibrate(200);
-        const container1 = document.getElementById(`${b1}`);
-        container1.classList.add('vibrate');
-        setTimeout(() => {
-            container1.classList.remove('vibrate');
-        }, 400);
-        const container2 = document.getElementById(`${b2}`);
-        container2.classList.add('vibrate');
-        setTimeout(() => {
-            container2.classList.remove('vibrate');
-        }, 400);
+        for(let i=0; i<args.length; i++){
+            const container = document.getElementById(`${args[i]}`);
+            container.classList.add('vibrate');
+            setTimeout(() => {
+              container.classList.remove('vibrate');
+            }, 400);
+        }
         return false;
     }
     return true;
@@ -52,19 +75,19 @@ function celebration_effect(){
 }
 
 document.getElementById("calculate_button").addEventListener('click',()=>{
-    result_mark=[0,0,false,false,false,false,0,0];
+    result_mark = [0, 0, 1, 0, 0, 0, 0, 0];
     document.getElementById('special_case_text').innerHTML=``;
     let m1_mark=parseInt(document.getElementById('mark_m1').value)
     let m2_mark=parseInt(document.getElementById('mark_m2').value)
     let m3_mark=parseInt(document.getElementById('mark_m3').value)
-    if(input_box_error_handler(m1_mark,'mark_m1') && input_box_error_handler(m2_mark,'mark_m2') && input_box_error_handler(m3_mark,'mark_m3') && radio_button_checker('yes_bonus','no_bonus') && radio_button_checker('nptel_yes','nptel_no') && radio_button_checker('course_yes','course_no') &&radio_button_checker('extra_yes','extra_no')){
+    if(input_box_error_handler(m1_mark,'mark_m1') && input_box_error_handler(m2_mark,'mark_m2') && input_box_error_handler(m3_mark,'mark_m3') && radio_button_checker('yes_bonus','no_bonus') && radio_button_checker('nptel_0','nptel_4', 'nptel_8') && radio_button_checker('course_yes','course_no') &&radio_button_checker('extra_yes','extra_no')){
         console.log(m1_mark);
         const bonus=document.getElementsByName('bonus');
         bonus.forEach((element)=>{
             if(element.checked){
                 if(element.value==='yes'){
                     result_mark[2]=true;
-                    }
+                }
             }
         })
         main(m1_mark,m2_mark,m3_mark);
@@ -89,22 +112,21 @@ function extra_activity_cal(){
     const extra=document.getElementsByName('extra');
     nptel_buttons.forEach((element)=>{
         if(element.checked){
-            if(element.value==='yes'){
-                result_mark[3]=true;
-            }
+            const NPTEL_SCORE = parseInt(element.value);
+            result_mark[3] = NPTEL_SCORE; //update nptel in result array
         }
     })
     course.forEach((element)=>{
         if(element.checked){
             if(element.value==='yes'){
-                result_mark[4]=true;
+                result_mark[4] = 7;           // update online certi in result array
             }
         }
     })
     extra.forEach((element)=>{
         if(element.checked){
             if(element.value==='yes'){
-                result_mark[5]=true;
+                result_mark[5] = 5;       // update extra work in result array
             }
         }
     })
@@ -112,14 +134,15 @@ function extra_activity_cal(){
 //first_10,second_10,bonus,nptel,course,extra,final_result,external
 function internal_mark_calculation(){
     let result=result_mark[0]+result_mark[1];
-    if(result_mark[3]){
-        result+=8;
+    // console.log("UR NPTEL MARK = ", result_mark[3])
+    if(result_mark[3]){ //nptel
+        result += result_mark[3];
     }
-    if(result_mark[4]){
-        result+=7;
+    if(result_mark[4]){ //courrse (by default result_mark[4] is 0, if(0) -> false)
+        result += result_mark[4];
     }
-    if(result_mark[5]){
-        result+=5;
+    if(result_mark[5]){ //competition(extra)
+        result += result_mark[5];
     }
     result_mark[6]=parseFloat(result.toFixed(2));
     external_mark_calculation()
@@ -136,9 +159,10 @@ function external_mark_calculation(){ //calcualtes the external mark
 
 function display_changer(){
     let bonus_or_not=(result_mark[2])?"WITH BONUS":"WITHOUT BONUS";
-    let nptel=(result_mark[3])?"8":"0";
-    let extra=(result_mark[5])?"5":"0";
-    let course=(result_mark[4])?"7":"0";
+    let nptel = result_mark[3];
+    let course = result_mark[4];
+    let extra = result_mark[5];
+
     document.querySelector('.result-mark-container').style.visibility='visible';
     document.querySelector('.result-mark-container').innerHTML=`<div class="row">
                     <p class="result-mark-header">${bonus_or_not}</p>
@@ -188,7 +212,7 @@ function dbStore(){
         },
         body: JSON.stringify({
             mark: result_mark[6],
-            nptel: result_mark[3] ? "yes" : "no",
+            nptel: result_mark[3] ,
             bonus: result_mark[2] ? "yes" : "no"
         }),
     })
