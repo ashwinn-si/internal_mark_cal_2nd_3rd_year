6070e9ec31a70e3b6a295f045ce3ff15625edcf9
ashwinn-si <ashwin23cse@gmail.com>
2026-03-31 10:22:59 +0530

    integer changed to float

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
                  ...
 */
 
-
 let result_mark = {
   first_10: 0,
   second_10: 0,
@@ -15,25 +14,25 @@ let result_mark = {
   pt3: 0,
   final_result: 0,
   external: 0,
-  category: 'general'
+  category: 'general',
 };
 
 function category_changer() {
-  const hopeElite = document.getElementById("cat_hope_elite").checked;
-  const pep = document.getElementById("cat_pep").checked;
+  const hopeElite = document.getElementById('cat_hope_elite').checked;
+  const pep = document.getElementById('cat_pep').checked;
 
-  document.getElementById("container_hope_elite").style.display = "none";
-  document.getElementById("container_pep").style.display = "none";
-  document.getElementById("container_general").style.display = "none";
+  document.getElementById('container_hope_elite').style.display = 'none';
+  document.getElementById('container_pep').style.display = 'none';
+  document.getElementById('container_general').style.display = 'none';
 
   if (hopeElite) {
-    document.getElementById("container_hope_elite").style.display = "block";
+    document.getElementById('container_hope_elite').style.display = 'block';
     result_mark.category = 'hope_elite';
   } else if (pep) {
-    document.getElementById("container_pep").style.display = "block";
+    document.getElementById('container_pep').style.display = 'block';
     result_mark.category = 'pep';
   } else {
-    document.getElementById("container_general").style.display = "block";
+    document.getElementById('container_general').style.display = 'block';
     result_mark.category = 'general';
   }
 }
@@ -54,14 +53,16 @@ function input_box_error_handler(mark, element_id, max_val = 100) {
 function radio_button_checker(...args) {
   let isINVALID = true;
 
-  for (let i = 0; i < args.length; i++) {   // check if any button is checked
+  for (let i = 0; i < args.length; i++) {
+    // check if any button is checked
     if (document.getElementById(args[i]).checked) {
       isINVALID = false;
       break;
     }
   }
 
-  if (isINVALID) {  // enters when there is no button checked.
+  if (isINVALID) {
+    // enters when there is no button checked.
     navigator.vibrate(200);
     for (let i = 0; i < args.length; i++) {
       const container = document.getElementById(`${args[i]}`);
@@ -77,79 +78,86 @@ function radio_button_checker(...args) {
 
 // Send analytics request on page load
 function sendAnalyticsRequest() {
-  let trafficType = window.trafficType || "normal";
-  fetch(`https://api.ashwinsi.in/personal-server/website/add-analytics?trafficType=${trafficType}&website=internalmarkcalculator`, {
-    method: "GET"
-  });
+  let trafficType = window.trafficType || 'normal';
+  fetch(
+    `https://api.ashwinsi.in/personal-server/website/add-analytics?trafficType=${trafficType}&website=internalmarkcalculator`,
+    {
+      method: 'GET',
+    }
+  );
 }
-window.addEventListener("DOMContentLoaded", sendAnalyticsRequest);
+window.addEventListener('DOMContentLoaded', sendAnalyticsRequest);
 
 function celebration_effect() {
-  document.getElementById("congrs-lottie-animation").style.visibility = 'visible';
+  document.getElementById('congrs-lottie-animation').style.visibility = 'visible';
   if (window.innerWidth > 768) {
-    document.getElementById("congrs-lottie-animation").classList.add('add_width_pc');
+    document.getElementById('congrs-lottie-animation').classList.add('add_width_pc');
   } else {
-    document.getElementById("congrs-lottie-animation").classList.add('add_width_mobile');
+    document.getElementById('congrs-lottie-animation').classList.add('add_width_mobile');
   }
   setInterval(() => {
-    document.getElementById("congrs-lottie-animation").style.visibility = 'hidden';
+    document.getElementById('congrs-lottie-animation').style.visibility = 'hidden';
     if (window.innerWidth > 768) {
-      document.getElementById("congrs-lottie-animation").classList.remove('add_width_pc');
+      document.getElementById('congrs-lottie-animation').classList.remove('add_width_pc');
     } else {
-      document.getElementById("congrs-lottie-animation").classList.remove('add_width_mobile');
+      document.getElementById('congrs-lottie-animation').classList.remove('add_width_mobile');
     }
   }, 3000);
 }
 
 function mark_calculator(m1_mark, m2_mark, m3_mark, bonus_flag) {
-  let bonus_mark = (bonus_flag) ? 1.5 : 1;
-  let first_10 = ((m1_mark * bonus_mark) + (m2_mark * bonus_mark)) * 0.05;
-  let second_10 = (m3_mark * bonus_mark) * 0.1;
-  first_10 = (first_10 > 10) ? 10 : first_10;
-  second_10 = (second_10 > 10) ? 10 : second_10;
+  let bonus_mark = bonus_flag ? 1.5 : 1;
+  let first_10 = (m1_mark * bonus_mark + m2_mark * bonus_mark) * 0.05;
+  let second_10 = m3_mark * bonus_mark * 0.1;
+  first_10 = first_10 > 10 ? 10 : first_10;
+  second_10 = second_10 > 10 ? 10 : second_10;
   result_mark.first_10 = parseFloat(first_10.toFixed(2));
   result_mark.second_10 = parseFloat(second_10.toFixed(2));
 }
 
 function extra_activity_cal() {
-  const hopeElite = document.getElementById("cat_hope_elite").checked;
-  const pep = document.getElementById("cat_pep").checked;
+  const hopeElite = document.getElementById('cat_hope_elite').checked;
+  const pep = document.getElementById('cat_pep').checked;
 
   if (hopeElite) {
     result_mark.category = 'hope_elite';
-    result_mark.pt1 = parseInt(document.getElementById("he_cert").value) || 0;
-    result_mark.pt2 = parseInt(document.getElementById("he_assessment").value) || 0;
-    result_mark.pt3 = parseInt(document.getElementById("he_perf").value) || 0;
+    result_mark.pt1 = parseFloat(document.getElementById('he_cert').value) || 0;
+    result_mark.pt2 = parseFloat(document.getElementById('he_assessment').value) || 0;
+    result_mark.pt3 = parseFloat(document.getElementById('he_perf').value) || 0;
   } else if (pep) {
     result_mark.category = 'pep';
-    result_mark.pt1 = parseInt(document.getElementById("pep_cert").value) || 0;
-    result_mark.pt2 = parseInt(document.getElementById("pep_assessment").value) || 0;
-    result_mark.pt3 = parseInt(document.getElementById("pep_nptel").value) || 0;
+    result_mark.pt1 = parseFloat(document.getElementById('pep_cert').value) || 0;
+    result_mark.pt2 = parseFloat(document.getElementById('pep_assessment').value) || 0;
+    result_mark.pt3 = parseFloat(document.getElementById('pep_nptel').value) || 0;
   } else {
     result_mark.category = 'general';
-    result_mark.pt1 = parseInt(document.getElementById("gen_nptel").value) || 0;
-    result_mark.pt2 = parseInt(document.getElementById("gen_cert").value) || 0;
-    result_mark.pt3 = parseInt(document.getElementById("gen_extra").value) || 0;
+    result_mark.pt1 = parseFloat(document.getElementById('gen_nptel').value) || 0;
+    result_mark.pt2 = parseFloat(document.getElementById('gen_cert').value) || 0;
+    result_mark.pt3 = parseFloat(document.getElementById('gen_extra').value) || 0;
   }
 }
 
 //first_10,second_10,bonus,nptel,course,extra,final_result,external
 function internal_mark_calculation() {
   let result = result_mark.first_10 + result_mark.second_10;
-  if (result_mark.pt1) { //nptel
+  if (result_mark.pt1) {
+    //nptel
     result += result_mark.pt1;
   }
-  if (result_mark.pt2) { //courrse (by default result_mark[4] is 0, if(0) -> false)
+  if (result_mark.pt2) {
+    //courrse (by default result_mark[4] is 0, if(0) -> false)
     result += result_mark.pt2;
   }
-  if (result_mark.pt3) { //competition(extra)
+  if (result_mark.pt3) {
+    //competition(extra)
     result += result_mark.pt3;
   }
   result_mark.final_result = parseFloat(result.toFixed(2));
-  external_mark_calculation()
+  external_mark_calculation();
 }
 
-function external_mark_calculation() { //calcualtes the external mark
+function external_mark_calculation() {
+  //calcualtes the external mark
   let external_mark = 91;
   if (result_mark.final_result >= 23) {
     external_mark = 45;
@@ -160,24 +168,33 @@ function external_mark_calculation() { //calcualtes the external mark
 }
 
 function display_changer() {
-  let bonus_or_not = (result_mark.bonus) ? "WITH BONUS" : "WITHOUT BONUS";
+  let bonus_or_not = result_mark.bonus ? 'WITH BONUS' : 'WITHOUT BONUS';
   let pt1 = result_mark.pt1;
   let pt2 = result_mark.pt2;
   let pt3 = result_mark.pt3;
 
   let label1, label2, label3, max1, max2, max3;
   if (result_mark.category === 'hope_elite') {
-    label1 = "CERTIFICATION"; max1 = 7;
-    label2 = "ASSESSMENT"; max2 = 6;
-    label3 = "PERFORMANCE"; max3 = 7;
+    label1 = 'CERTIFICATION';
+    max1 = 7;
+    label2 = 'ASSESSMENT';
+    max2 = 6;
+    label3 = 'PERFORMANCE';
+    max3 = 7;
   } else if (result_mark.category === 'pep') {
-    label1 = "CERT / COURSE"; max1 = 7;
-    label2 = "ASSESSMENT"; max2 = 6;
-    label3 = "NPTEL"; max3 = 7;
+    label1 = 'CERT / COURSE';
+    max1 = 7;
+    label2 = 'ASSESSMENT';
+    max2 = 6;
+    label3 = 'NPTEL';
+    max3 = 7;
   } else {
-    label1 = "NPTEL"; max1 = 8;
-    label2 = "COURSE"; max2 = 7;
-    label3 = "EXTRA"; max3 = 5;
+    label1 = 'NPTEL';
+    max1 = 8;
+    label2 = 'COURSE';
+    max2 = 7;
+    label3 = 'EXTRA';
+    max3 = 5;
   }
 
   document.querySelector('.result-mark-container').style.visibility = 'visible';
@@ -212,29 +229,29 @@ function display_changer() {
                     <div class="col"><p>MARK TO PASS </p></div>
                     <div class="col"><p>${result_mark.external} / 100</p></div>
                 </div>
-                <button id="possible-mark-button">POSSIBLE MARK</button>`
+                <button id="possible-mark-button">POSSIBLE MARK</button>`;
   document.getElementById('possible-mark-button').addEventListener('click', () => {
     let intermal_marl_scored = result_mark.final_result;
     localStorage.setItem('internal_mark', JSON.stringify(intermal_marl_scored));
-    window.location.href = "result_page.html";
-    window.location.href = "result_page/result_page.html";
+    window.location.href = 'result_page.html';
+    window.location.href = 'result_page/result_page.html';
   });
 }
-
 
 function main(m1_mark, m2_mark, m3_mark) {
   mark_calculator(m1_mark, m2_mark, m3_mark, result_mark.bonus);
   if (m1_mark + m2_mark + m3_mark >= 100) {
     extra_activity_cal();
   } else {
-    document.getElementById('special_case_text').innerHTML = `<p style=" background-color: tomato;width: 150px; padding: 3px;border-radius: 5px; font-weight: bold; color: white; box-shadow: 0 2px 4px rgba(0, 0, 0, 1);">SPECIAL CASE</p>`;
+    document.getElementById('special_case_text').innerHTML =
+      `<p style=" background-color: tomato;width: 150px; padding: 3px;border-radius: 5px; font-weight: bold; color: white; box-shadow: 0 2px 4px rgba(0, 0, 0, 1);">SPECIAL CASE</p>`;
   }
   internal_mark_calculation();
   display_changer();
   dbStore(m1_mark, m2_mark, m3_mark);
 }
 
-document.getElementById("calculate_button").addEventListener("click", () => {
+document.getElementById('calculate_button').addEventListener('click', () => {
   result_mark = {
     first_10: 0,
     second_10: 0,
@@ -244,53 +261,52 @@ document.getElementById("calculate_button").addEventListener("click", () => {
     pt3: 0,
     final_result: 0,
     external: 0,
-    category: 'general'
+    category: 'general',
   };
-  document.getElementById("special_case_text").innerHTML = ``;
-  let m1_mark = parseInt(document.getElementById("mark_m1").value);
-  let m2_mark = parseInt(document.getElementById("mark_m2").value);
-  let m3_mark = parseInt(document.getElementById("mark_m3").value);
+  document.getElementById('special_case_text').innerHTML = ``;
+  let m1_mark = parseFloat(document.getElementById('mark_m1').value);
+  let m2_mark = parseFloat(document.getElementById('mark_m2').value);
+  let m3_mark = parseFloat(document.getElementById('mark_m3').value);
 
   let isValid = true;
-  isValid = input_box_error_handler(m1_mark, "mark_m1", 100) && isValid;
-  isValid = input_box_error_handler(m2_mark, "mark_m2", 100) && isValid;
-  isValid = input_box_error_handler(m3_mark, "mark_m3", 100) && isValid;
-  isValid = radio_button_checker("yes_bonus", "no_bonus") && isValid;
+  isValid = input_box_error_handler(m1_mark, 'mark_m1', 100) && isValid;
+  isValid = input_box_error_handler(m2_mark, 'mark_m2', 100) && isValid;
+  isValid = input_box_error_handler(m3_mark, 'mark_m3', 100) && isValid;
+  isValid = radio_button_checker('yes_bonus', 'no_bonus') && isValid;
 
-  const hopeElite = document.getElementById("cat_hope_elite").checked;
-  const pep = document.getElementById("cat_pep").checked;
+  const hopeElite = document.getElementById('cat_hope_elite').checked;
+  const pep = document.getElementById('cat_pep').checked;
 
   if (hopeElite) {
-    let pt1 = parseInt(document.getElementById("he_cert").value) || 0;
-    let pt2 = parseInt(document.getElementById("he_assessment").value) || 0;
-    let pt3 = parseInt(document.getElementById("he_perf").value) || 0;
-    isValid = input_box_error_handler(pt1, "he_cert", 7) && isValid;
-    isValid = input_box_error_handler(pt2, "he_assessment", 6) && isValid;
-    isValid = input_box_error_handler(pt3, "he_perf", 7) && isValid;
+    let pt1 = parseFloat(document.getElementById('he_cert').value) || 0;
+    let pt2 = parseFloat(document.getElementById('he_assessment').value) || 0;
+    let pt3 = parseFloat(document.getElementById('he_perf').value) || 0;
+    isValid = input_box_error_handler(pt1, 'he_cert', 7) && isValid;
+    isValid = input_box_error_handler(pt2, 'he_assessment', 6) && isValid;
+    isValid = input_box_error_handler(pt3, 'he_perf', 7) && isValid;
   } else if (pep) {
-    let pt1 = parseInt(document.getElementById("pep_cert").value) || 0;
-    let pt2 = parseInt(document.getElementById("pep_assessment").value) || 0;
-    let pt3 = parseInt(document.getElementById("pep_nptel").value) || 0;
-    isValid = input_box_error_handler(pt1, "pep_cert", 7) && isValid;
-    isValid = input_box_error_handler(pt2, "pep_assessment", 6) && isValid;
-    isValid = input_box_error_handler(pt3, "pep_nptel", 7) && isValid;
+    let pt1 = parseFloat(document.getElementById('pep_cert').value) || 0;
+    let pt2 = parseFloat(document.getElementById('pep_assessment').value) || 0;
+    let pt3 = parseFloat(document.getElementById('pep_nptel').value) || 0;
+    isValid = input_box_error_handler(pt1, 'pep_cert', 7) && isValid;
+    isValid = input_box_error_handler(pt2, 'pep_assessment', 6) && isValid;
+    isValid = input_box_error_handler(pt3, 'pep_nptel', 7) && isValid;
   } else {
-    let pt1 = parseInt(document.getElementById("gen_nptel").value) || 0;
-    let pt2 = parseInt(document.getElementById("gen_cert").value) || 0;
-    let pt3 = parseInt(document.getElementById("gen_extra").value) || 0;
-    isValid = input_box_error_handler(pt1, "gen_nptel", 8) && isValid;
-    isValid = input_box_error_handler(pt2, "gen_cert", 7) && isValid;
-    isValid = input_box_error_handler(pt3, "gen_extra", 5) && isValid;
+    let pt1 = parseFloat(document.getElementById('gen_nptel').value) || 0;
+    let pt2 = parseFloat(document.getElementById('gen_cert').value) || 0;
+    let pt3 = parseFloat(document.getElementById('gen_extra').value) || 0;
+    isValid = input_box_error_handler(pt1, 'gen_nptel', 8) && isValid;
+    isValid = input_box_error_handler(pt2, 'gen_cert', 7) && isValid;
+    isValid = input_box_error_handler(pt3, 'gen_extra', 5) && isValid;
   }
 
   if (isValid) {
-    const bonus = document.getElementsByName("bonus");
-    bonus.forEach((element) => {
+    const bonus = document.getElementsByName('bonus');
+    bonus.forEach(element => {
       if (element.checked) {
-        if (element.value === "yes") {
+        if (element.value === 'yes') {
           result_mark.bonus = true;
-        }
-        else {
+        } else {
           result_mark.bonus = false;
         }
       }
@@ -300,12 +316,11 @@ document.getElementById("calculate_button").addEventListener("click", () => {
   }
 });
 
-
 function dbStore(m1_mark, m2_mark, m3_mark) {
-  fetch("https://api.ashwinsi.in/personal-server/internalMark/addMark", {
-    method: "POST",
+  fetch('https://api.ashwinsi.in/personal-server/internalMark/addMark', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       mark: result_mark.final_result,
@@ -313,12 +328,12 @@ function dbStore(m1_mark, m2_mark, m3_mark) {
       pt1: result_mark.pt1,
       pt2: result_mark.pt2,
       pt3: result_mark.pt3,
-      bonus: result_mark.bonus ? "yes" : "no",
+      bonus: result_mark.bonus ? 'yes' : 'no',
       m1Mark: m1_mark,
       m2Mark: m2_mark,
-      m3Mark: m3_mark
+      m3Mark: m3_mark,
     }),
-  })
+  });
 }
 
 //theme changer
@@ -329,12 +344,11 @@ const themeToggle = document.querySelector('.switch .input');
 function toggleTheme() {
   let newTheme;
 
-
   if (themeToggle.checked) {
     newTheme = 'dark';
   } else {
     const currentTheme = document.documentElement.getAttribute('data-theme');
-    newTheme = "light";
+    newTheme = 'light';
   }
   document.documentElement.setAttribute('data-theme', newTheme);
 }
@@ -344,5 +358,3 @@ themeToggle.addEventListener('change', toggleTheme);
 
 // Initialize the theme on page load
 toggleTheme();
-
-
